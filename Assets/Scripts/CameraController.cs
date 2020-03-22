97a7f52e1efcd4d135ddcfee0ab16fe2f6bdca53
Yusuke Kawamoto <novogrammer@gmail.com>
2020-03-22 18:49:03 +0900

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera camera;
+    public GameObject VirtualCameraA;
+    public GameObject VirtualCameraB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.camera = GetComponent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 positionA = VirtualCameraA.transform.localPosition;
+        Quaternion rotationA = VirtualCameraA.transform.localRotation;
+        Vector3 positionB = VirtualCameraB.transform.localPosition;
+        Quaternion rotationB = VirtualCameraB.transform.localRotation;
+
+        float time = Time.time;
+        float r = Mathf.Sin(time)*0.5f+0.5f ;
+        Vector3 position = Vector3.Lerp(positionA, positionB, r);
+        Quaternion rotation = Quaternion.Slerp(rotationA, rotationB, r);
+
+
+        this.transform.SetPositionAndRotation(position, rotation);
+    }
+}
+;
---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * TODO:
+ * 二点を移動する、二次ベジエなどが必要かもしれない。
+ * メトロノーム機能、タップ機能
+ * githubへプロジェクトを登録する
+ * 二台目追加、シリアルナンバーを指定すると可能
+ * ポイントクラウドをカスタムする
+ * 
+ * 
+ */
+
+
+
+public class Metronome : MonoBehaviour
+{
+    public double bpm=120;
+    public int beatCountQty = 32;
+    private int beatCount;
+    private double timeFromPreviousBeat;
+    
+    void Start()
+    {
+        
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        var spb = this.GetSPB();
+        this.timeFromPreviousBeat += Time.fixedDeltaTime;
+        if (spb <= this.timeFromPreviousBeat)
+        {
+            this.beatCount=(this.beatCount+1)%this.beatCountQty;
+
+        }
+        
+    }
+
+
+    private double GetSPB()
+    {
+        if (this.bpm<=0.0)
+        {
+            return double.MaxValue;
+        }
+        return 60.0/this.bpm;
+    }
+
+}
