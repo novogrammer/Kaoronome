8e85f15c200f1dab303735727ad348cae5602384
Yusuke Kawamoto <novogrammer@gmail.com>
2020-04-11 18:12:49 +0900

    Metronomeクラスを作成、Cubeも追加

---
+++ Assets/Scripts/MetronomeHolder.cs
@@ -0,0 +1,63 @@
+﻿using System .Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetronomeHolder : MonoBehaviour
+{
+    private float previousTapTime;
+
+    private const float BPM_MIN = 15.0f;
+    private const float BPM_MAX = 500.0f;
+
+    private Metronome metronome;
+
+    public struct MetronomeSnapshot
+    {
+        public float BPM;
+        public float SPB;
+        public int BeatCount;
+        public float BeatProgress;
+        public float TimeFromPreviousBeat;
+
+    }
+    public MetronomeSnapshot GetMetronomeSnapshot()
+    {
+        MetronomeSnapshot metronomeSnapshot = new MetronomeSnapshot()
+        {
+            BPM = this.metronome.BPM,
+            SPB = this.metronome.SPB,
+            BeatCount = this.metronome.BeatCount,
+            BeatProgress = this.metronome.BeatProgress,
+            TimeFromPreviousBeat = this.metronome.TimeFromPreviousBeat,
+        };
+        return metronomeSnapshot;
+    }
+
+    void Start()
+    {
+        this.previousTapTime = Time.time;
+        this.metronome = new Metronome(60, 32);
+    }
+
+
+    void FixedUpdate()
+    {
+        this.metronome.Update(Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float time = Time.time;
+            float duration = time - this.previousTapTime;
+            float newBpm = 60.0f / duration;
+            if (BPM_MIN <= newBpm && newBpm <= BPM_MAX)
+            {
+                this.metronome.BPM = newBpm;
+            }
+            this.previousTapTime = time;
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Plain/Metronome.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome
+{
+    private float bpm;
+    private int beatCountQty;
+    private int beatCount;
+    private float timeFromPreviousBeat;
+
+    public float BPM
+    {
+        get { return this.bpm; }
+        set{
+            float newBpm = value;
+            if (0.5f<this.BeatProgress)
+            {
+                this.beatCount = (this.beatCount + 1) % this.beatCountQty;
+            }
+            this.bpm = newBpm;
+            Debug.Log("bpm:" + this.bpm);
+
+            this.timeFromPreviousBeat = 0.0f;
+
+        }
+    }
+    public float SPB
+    {
+        get
+        {
+            if (this.bpm <= 0.0)
+            {
+                return float.MaxValue;
+            }
+            return 60.0f / this.bpm;
+        }
+    }
+    public int BeatCount
+    {
+        get { return this.beatCount; }
+    }
+    public float BeatProgress
+    {
+        get { return this.TimeFromPreviousBeat / this.SPB; }
+    }
+    public float TimeFromPreviousBeat
+    {
+        get { return this.timeFromPreviousBeat; }
+    }
+
+
+    public Metronome(float bpm, int beatCountQty)
+    {
+        this.bpm = bpm;
+        this.beatCountQty = beatCountQty;
+
+    }
+    public void Update(float dt)
+    {
+        float spb = this.SPB;
+        this.timeFromPreviousBeat += dt;
+        if (spb <= this.timeFromPreviousBeat)
+        {
+            this.beatCount = (this.beatCount + 1) % this.beatCountQty;
+            this.timeFromPreviousBeat -= spb;
+        }
+
+    }
+}
